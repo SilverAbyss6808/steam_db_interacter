6bf6042492f4f7e98ba9a6ed8179eac3b6db92c0
l-baxter1 <l-baxter@onu.edu>
2025-03-10 11:29:28 -0400

    init

---
+++ api_access.py
@@ -0,0 +1,22 @@
+
+import requests
+
+api_key = 0x5B624547C66523E8850C121185F1E90B
+user = 76561199221076311  # me
+game = 1454400
+
+interface = 'ISteamUser'
+method = 'CheckAppOwnership'
+method_version = 'v4'
+
+# request format https://api.steampowered.com/<interface>/<method>/<method_version>/
+url = 'https://api.steampowered.com/' + interface + '/' + method + '/' + method_version + '/'
+
+request_parameters = {
+    'key': api_key,
+    'steamid': user,
+    'appid': game
+}
+
+response = requests.get(url, request_parameters)
+print(response.text)
